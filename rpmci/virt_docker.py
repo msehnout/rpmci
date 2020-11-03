e07b58e7e628169f16f6a3b67d7f05c2539a701d
David Rheinsberg <david.rheinsberg@gmail.com>
2020-11-03 15:25:53 +0100

    rpmci/virt_docker: use detached containers
    Rather than executing an ad-hoc container, turn the virtualization into
    a managed container that allows to execute a series of commands in a
    single instance.

--- rpmci/virt_docker.py
+++ rpmci/virt_docker.py
@@ -18,26 +18,34 @@ class VirtDocker(contextlib.AbstractContextManager):
     def __init__(
         self,
         exec_image,
+        exec_privileged,
     ):
         self._exec_image = exec_image
+        self._exec_privileged = exec_privileged
+        self._exec_ref = None
 
     def _image_acquire(self):
         # Pull the specified image and store it in the local image store.
         # Unfortunately, docker does not allow us to store it in our private
         # location, or tag it with our own tag, or somehow pin it. This means,
-        # we cannot race-free tell whether we are the only user of that image,
+        # we cannot tell race-free whether we are the only user of that image,
         # nor can we reliably pin the image for the time we need it. Any racing
         # maintenance work will interfere.
         # We are unaware of any workarounds, hence, we simply pull the image
-        # and leave it around. See `_image_release()` for more information.
+        # and leave it around.
 
         cmd = [
             "docker",
             "pull",
+            "--quiet",
             self._exec_image,
         ]
 
-        with util.manage_process(subprocess.Popen(cmd)) as proc:
+        with util.manage_process(subprocess.Popen(
+            cmd,
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+        )) as proc:
             res = proc.wait()
             if res != 0:
                 raise RuntimeError(f"Cannot pull specified docker image: {self._exec_image}: {res}")
@@ -51,31 +59,90 @@ class VirtDocker(contextlib.AbstractContextManager):
         # we simply never untag images for now.
         pass
 
+    def _container_start(self):
+        # Start a new adhoc container with the specified image. We require the
+        # default entrypoint to stall the container (i.e., quite likely some
+        # init implementation).
+        # We remember the container ID so we can properly stop the container
+        # when we no longer need it.
+
+        cmd = [
+            "docker",
+            "container",
+            "run",
+            "--detach",
+            "--rm",
+        ]
+
+        if self._exec_privileged:
+            cmd += ["--privileged"]
+
+        cmd += [self._exec_image]
+
+        with util.manage_process(subprocess.Popen(
+            cmd,
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.PIPE,
+        )) as proc:
+            stdout, _stderr = proc.communicate()
+            if proc.returncode != 0:
+                raise RuntimeError(f"Cannot run specified docker image: {self._exec_image}: {res}")
+
+        self._exec_ref = stdout.rstrip()
+
+    def _container_stop(self):
+        if self._exec_ref is None:
+            return
+
+        cmd = [
+            "docker",
+            "container",
+            "stop",
+            self._exec_ref,
+        ]
+
+        with util.manage_process(subprocess.Popen(
+            cmd,
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+        )) as proc:
+            proc.wait()
+            # We do not care whether the container was auto-cleaned or whether
+            # this cleaned it up. Ignore the return code for now.
+
+        self._exec_ref = None
+
     def __enter__(self):
         try:
             self._image_acquire()
+            self._container_start()
             return self
         except:
+            self._container_stop()
             self._image_release()
             raise
 
     def __exit__(self, exc_type, exc_value, exc_tb):
+        self._container_stop()
         self._image_release()
 
-    def run(self, args, *, privileged=False):
-        """Run new container instance"""
+    def run(self, args):
+        """Execute command in container"""
 
         cmd = [
-            "docker"
-            "run"
-            "--rm"
+            "docker",
+            "container",
+            "exec",
         ]
 
-        if privileged:
+        if self._exec_privileged:
             cmd += ["--privileged"]
 
-        cmd += [self._exec_image]
+        cmd += [self._exec_ref]
         cmd += args
 
-        with util.manage_process(subprocess.Popen(cmd)) as proc:
+        with util.manage_process(subprocess.Popen(
+            cmd,
+            stdin=subprocess.DEVNULL,
+        )) as proc:
             return proc.wait()
