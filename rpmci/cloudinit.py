05cdb33a0026461547213e88717680e64dcc612c
Martin Sehnoutka <sehnoutka.martin@gmail.com>
2020-11-09 13:40:00 +0100

    rpmci/cloudinit: Create a class for creating customized cloud-init files
    The previous version used predefined structure which was nice for PoC,
    but it not well suited for further development. Encapsulating the same
    logic into a class enable us to easily generate different cloud-init
    files for steering and target VMs.

--- rpmci/cloudinit.py
+++ rpmci/cloudinit.py
@@ -2,107 +2,157 @@ import base64
 import logging
 import subprocess
 
+from pathlib import Path
+
 import yaml
 
 
-def write_metadata_file(filename):
-    """Write meta-data file for cloud-init."""
-    logging.info("Writing meta-data file")
-    with open(filename, "w") as f:
-        print("instance-id: nocloud", file=f)
-        print("local-hostname: vm", file=f)
+class CloudInit:
+    def __init__(self):
+        self.repos = {}
+        self.users = []
+        self.ssh_keypair = None
+        self.ssh_configs = {}
 
+    def add_repo(self, name: str, baseurl: str):
+        self.repos[name] = {
+            "name": name,
+            "baseurl": baseurl,
+            "enabled": True,
+            "gpgcheck": False,
+        }
 
-def userdata(pubkey_filename, privkey_filename, repo_baseurl, vm_dict):
-    logging.info("Generating user-data")
-    with open(pubkey_filename, 'r') as f:
-        pubkey_string = f.read()
-    with open(privkey_filename, 'r') as f:
-        privkey_string = f.read()
-    user_data = {
-        "yum_repos": {
-            "osbuild": {
-                "name": "osbuild",
-                "baseurl": repo_baseurl,
-                "enabled": True,
-                "gpgcheck": False,
-            }
-        },
-        "user": "admin",
-        "password": "foobar",
-        "ssh_authorized_keys": [
-            pubkey_string
-        ],
-        "ssh_pwauth": "True",
-        "chpasswd": {
-            "expire": False,
-        },
-        "sudo": "ALL=(ALL) NOPASSWD:ALL",
-        "write_files": [
-            {
-                "path": "/etc/ssh/id_rsa.pub",
-                "encoding": "b64",
-                "content": base64.b64encode(pubkey_string.encode("utf-8")).decode("utf-8"),
-                "permissions": "0644",
+    def add_user(self, username: str, password: str, ssh_pubkey: str):
+        self.users += [{
+            "user": username,
+            "password": password,
+            "ssh_authorized_keys": [ssh_pubkey],
+            "ssh_pwauth": True,
+            "chpasswd": {
+                "expire": False,
             },
-            {
-                "path": "/etc/ssh/id_rsa",
-                "encoding": "b64",
-                "content": base64.b64encode(privkey_string.encode("utf-8")).decode("utf-8"),
-                "permissions": "0644",
-            },
-            {
-                "path": "/etc/ssh/ssh_config",
-                "encoding": "b64",
-                "content": base64.b64encode(f"""Host testvm
-         HostName {vm_dict["testvm"]["ip"]}
-         User admin
-         Port {vm_dict["testvm"]["port"]}
-         IdentityFile /etc/ssh/id_rsa
-         StrictHostKeyChecking no
-    Host targetvm
-         HostName {vm_dict["targetvm"]["ip"]}
-         User admin
-         Port {vm_dict["targetvm"]["port"]}
-         IdentityFile /etc/ssh/id_rsa
-         StrictHostKeyChecking no
-                    """.encode("utf-8")).decode("utf-8"),
-                "permissions": "0644",
-            }
-        ]
-    }
-    return user_data
+            "sudo": "ALL=(ALL) NOPASSWD:ALL",
+        }]
+
+    def add_ssh_key_pair(self, public_key: str, private_key: str):
+        self.ssh_keypair = {
+            "public_key": public_key,
+            "private_key": private_key,
+        }
+
+    def add_ssh_config(self, host_alias: str, hostname: str, port: int, username: str):
+        """Create new entry in /etc/ssh_config.
+
+        Parameters
+        ----------
+        host_alias The name that the user can use to connect to this machine.
+        hostname Domain name or IP address of the machine.
+        port Port where the SSH daemon listens.
+        username User with known password or SSH key.
+        """
+        self.ssh_configs[host_alias] = {
+            "HostName": hostname,
+            "Port": port,
+            "User": username,
+            "IdentityFile": "/etc/ssh/id_rsa",
+            "StrictHostKeyChecking": "no",
+        }
+
+    def get_userdata_str(self):
+        write_files = []
+        user_data = {}
+        if len(self.repos.keys()) > 0:
+            user_data["yum_repos"] = self.repos
+        if len(self.users) > 0:
+            user_data["users"] = self.users
+        if self.ssh_keypair is not None:
+            write_files += [
+                {
+                    "path": "/etc/ssh/id_rsa.pub",
+                    "encoding": "b64",
+                    "content": base64.b64encode(self.ssh_keypair["public_key"].encode("utf-8")).decode("utf-8"),
+                    "permissions": "0644",
+                },
+                {
+                    "path": "/etc/ssh/id_rsa",
+                    "encoding": "b64",
+                    "content": base64.b64encode(self.ssh_keypair["private_key"].encode("utf-8")).decode("utf-8"),
+                    "permissions": "0644",
+                }
+            ]
+        if len(self.ssh_configs.keys()) > 0:
+            ssh_config_content = ""
+            for host, config in self.ssh_configs.items():
+                ssh_config_content += f"Host {host}\n"
+                for k,v in config.items():
+                    ssh_config_content += f"    {k} {v}\n"
+            write_files += [
+                {
+                    "path": "/etc/ssh/ssh_config",
+                    "encoding": "b64",
+                    "content": base64.b64encode(ssh_config_content.encode("utf-8")).decode("utf-8"),
+                    "permissions": "0644",
+                }
+            ]
+        if len(write_files) > 0:
+            user_data["write_files"] = write_files
+
+        user_data_str = yaml.dump(user_data, Dumper=yaml.SafeDumper)
+        return f"#cloud-config\n{user_data_str}"
+
+    @staticmethod
+    def _write_userdata_file(filename: Path, content: str):
+        """Write user-data file for cloud-init."""
+        logging.info("Writing user-data file")
+        with open(filename, "w") as f:
+            f.write(content)
+
+    @staticmethod
+    def _write_metadata_file(filename: Path):
+        """Write meta-data file for cloud-init."""
+        logging.info("Writing meta-data file")
+        with open(filename, "w") as f:
+            print("instance-id: nocloud", file=f)
+            print("local-hostname: vm", file=f)
+
+    def get_iso(self, cache_dir: Path, iso_file_name: str) -> Path:
+        logging.info("Generating cloud-init ISO file")
+        cloudinit_file = cache_dir.joinpath(iso_file_name)
+        userdata_file = cache_dir.joinpath("user-data")
+        self._write_userdata_file(userdata_file, self.get_userdata_str())
+        metadata_file = cache_dir.joinpath("meta-data")
+        self._write_metadata_file(metadata_file)
+        # Create an ISO that cloud-init can consume with userdata.
+        subprocess.run(["genisoimage",
+                        "-quiet",
+                        "-input-charset", "utf-8",
+                        "-output", cloudinit_file,
+                        "-volid", "cidata",
+                        "-joliet",
+                        "-rock",
+                        "-quiet",
+                        "-graft-points",
+                        userdata_file,
+                        metadata_file],
+                       check=True)
+        return cloudinit_file
 
 
-def write_userdata_file(filename, pubkey_filename, privkey_filename, repo_baseurl, vm_dict):
-    """Write user-data file for cloud-init."""
-    user_data = userdata(pubkey_filename, privkey_filename, repo_baseurl, vm_dict)
-    logging.info("Writing user-data file")
-    with open(filename, "w") as f:
-        print("#cloud-config", file=f)
-        yaml.dump(user_data, f, Dumper=yaml.SafeDumper)
-
-
-def write_userdata_str(pubkey_filename, privkey_filename, repo_baseurl, vm_dict):
-    """Write user-data to a string."""
-    user_data = userdata(pubkey_filename, privkey_filename, repo_baseurl, vm_dict)
-    userdatastr = yaml.dump(user_data, Dumper=yaml.SafeDumper)
-    return f"#cloud-config\n{userdatastr}"
-
-
-def create_cloudinit_iso(userdata_file, metadata_file, cloudinit_file):
-    """Take the user-data and meta-data files and generate cloud-init iso from them."""
-    logging.info("Generating cloud-init ISO file")
-    # Create an ISO that cloud-init can consume with userdata.
-    subprocess.run(["genisoimage",
-                    "-quiet",
-                    "-input-charset", "utf-8",
-                    "-output", cloudinit_file,
-                    "-volid", "cidata",
-                    "-joliet",
-                    "-rock",
-                    "-quiet",
-                    "-graft-points",
-                    userdata_file,
-                    metadata_file],
-                   check=True)
+def test_CloudInit_get_userdata_str():
+    cloudinit = CloudInit()
+    cloudinit.add_repo("osbuild", "osbuild.org")
+    cloudinit.add_user("admin", "foobar", "abc")
+    cloudinit.add_ssh_key_pair("pubkey", "privkey")
+    cloudinit.add_ssh_config("target", "127.0.0.1", 2222, "admin")
+    resulting_string = cloudinit.get_userdata_str()
+    loaded_yaml = yaml.load(resulting_string, Loader=yaml.SafeLoader)
+    assert loaded_yaml["users"][0]["user"] == "admin"
+    generated_ssh_config = base64.b64decode(loaded_yaml["write_files"][2]["content"].encode("utf-8")).decode("utf-8")
+    expected_ssh_config = """Host target
+    HostName 127.0.0.1
+    Port 2222
+    User admin
+    IdentityFile /etc/ssh/id_rsa
+    StrictHostKeyChecking no
+"""
+    assert generated_ssh_config == expected_ssh_config
